f1bc7edc667b424ca773100bd4eb9b3c41591d7d
Nima Hamidi <hamidi@alumni.stanford.edu>
2022-01-04 11:24:32 -0600

    Move _CATALOGS to __init__.py

---
+++ pod/__init__.py
@@ -0,0 +1,15 @@
+from typing import Dict
+
+from .core import PodConverterCatalog
+from .bytes import _BYTES_CATALOG
+
+_CATALOGS: Dict[str, PodConverterCatalog] = {
+    "bytes": _BYTES_CATALOG,
+}
+
+
+def get_catalog(name):
+    """
+    Returns a converter catalog corresponding to name (e.g., for name="bytes" or name="json").
+    """
+    return _CATALOGS[name]
