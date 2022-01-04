b7c44757815bc7af0f77f2badc53e24a3c76f850
Nima Hamidi <hamidi@alumni.stanford.edu>
2022-01-04 11:31:40 -0600

    Introduce atomic types

---
+++ pod/types/atomic.py
@@ -0,0 +1,145 @@
+import struct
+
+from io import BytesIO
+from typing import Literal
+
+from ..decorators import pod
+
+_BYTEORDER: Literal["little", "big"] = "little"
+
+
+def new_atomic_type(name: str, base: type, code: str, unpacker, packer=lambda x: x):
+    @pod(override=True, dataclass_fn=None)
+    class Atom(base):  # type: ignore
+        @classmethod
+        def _get_code(cls):
+            order_char = "<" if _BYTEORDER == "little" else ">"
+            return code.format(order_char)
+
+        @classmethod
+        def _is_static(cls) -> bool:
+            return True
+
+        @classmethod
+        def _calc_max_size(cls):
+            return struct.calcsize(cls._get_code())
+
+        @classmethod
+        def _to_bytes_partial(cls, _type, buffer, obj):
+            obj = packer(obj)
+            buffer.write(struct.pack(cls._get_code(), obj))
+
+        @classmethod
+        def _from_bytes_partial(cls, _type, buffer: BytesIO):
+            size = cls._calc_max_size()
+            encoded = buffer.read(size)
+            decoded, *_ = struct.unpack(cls._get_code(), encoded)
+            return unpacker(decoded)
+
+    Atom.__name__ = name
+    Atom.__qualname__ = name
+
+    return Atom
+
+
+def set_default_repr(repr_code):
+    global _BYTEORDER
+    _BYTEORDER = repr_code
+
+
+def get_default_repr():
+    return _BYTEORDER
+
+
+# 1-byte integers
+I8l = new_atomic_type("I8l", int, "<b", int)
+I8b = new_atomic_type("I8b", int, ">b", int)
+I8 = new_atomic_type("I8", int, "{}b", int)
+
+U8l = new_atomic_type("I8l", int, "<B", int)
+U8b = new_atomic_type("I8b", int, ">B", int)
+U8 = new_atomic_type("I8", int, "{}B", int)
+
+# 2-byte integers
+I16l = new_atomic_type("I16l", int, "<h", int)
+I16b = new_atomic_type("I16b", int, ">h", int)
+I16 = new_atomic_type("I16", int, "{}h", int)
+
+U16l = new_atomic_type("I16l", int, "<H", int)
+U16b = new_atomic_type("I16b", int, ">H", int)
+U16 = new_atomic_type("I16", int, "{}H", int)
+
+
+# 4-byte integers
+I32l = new_atomic_type("I32l", int, "<i", int)
+I32b = new_atomic_type("I32b", int, ">i", int)
+I32 = new_atomic_type("I32", int, "{}i", int)
+
+U32l = new_atomic_type("I32l", int, "<I", int)
+U32b = new_atomic_type("I32b", int, ">I", int)
+U32 = new_atomic_type("I32", int, "{}I", int)
+
+
+# 8-byte integers
+I64l = new_atomic_type("I64l", int, "<q", int)
+I64b = new_atomic_type("I64b", int, ">q", int)
+I64 = new_atomic_type("I64", int, "{}q", int)
+
+U64l = new_atomic_type("I64l", int, "<Q", int)
+U64b = new_atomic_type("I64b", int, ">Q", int)
+U64 = new_atomic_type("I64", int, "{}Q", int)
+
+
+# 16-byte integers
+I128l = new_atomic_type(
+    "I128l",
+    int,
+    "16s",
+    unpacker=lambda x: int.from_bytes(x, byteorder="little", signed=True),
+    packer=lambda x: int.to_bytes(x, length=16, byteorder="little", signed=True),
+)
+I128b = new_atomic_type(
+    "I128b",
+    int,
+    "16s",
+    unpacker=lambda x: int.from_bytes(x, byteorder="big", signed=True),
+    packer=lambda x: int.to_bytes(x, length=16, byteorder="big", signed=True),
+)
+I128 = new_atomic_type(
+    "I128",
+    int,
+    "16s",
+    unpacker=lambda x: int.from_bytes(x, byteorder=_BYTEORDER, signed=True),
+    packer=lambda x: int.to_bytes(x, length=16, byteorder=_BYTEORDER, signed=True),
+)
+
+U128l = new_atomic_type(
+    "I128l",
+    int,
+    "16s",
+    unpacker=lambda x: int.from_bytes(x, byteorder="little", signed=False),
+    packer=lambda x: int.to_bytes(x, length=16, byteorder="little", signed=False),
+)
+U128b = new_atomic_type(
+    "I128b",
+    int,
+    "16s",
+    unpacker=lambda x: int.from_bytes(x, byteorder="big", signed=False),
+    packer=lambda x: int.to_bytes(x, length=16, byteorder="big", signed=False),
+)
+U128 = new_atomic_type(
+    "I128",
+    int,
+    "16s",
+    unpacker=lambda x: int.from_bytes(x, byteorder=_BYTEORDER, signed=False),
+    packer=lambda x: int.to_bytes(x, length=16, byteorder=_BYTEORDER, signed=False),
+)
+
+# Floating-point
+F32l = new_atomic_type("F32l", float, "<f", float)
+F32b = new_atomic_type("F32b", float, ">f", float)
+F32 = new_atomic_type("F32", float, "{}f", float)
+
+F64l = new_atomic_type("F64l", float, "<d", float)
+F64b = new_atomic_type("F64b", float, ">d", float)
+F64 = new_atomic_type("F64", float, "{}d", float)
---
+++ tests/unit/test_atomic.py
@@ -0,0 +1,75 @@
+from pod.types.atomic import I8, U8, I32l, I32b, U32l, U32b, I128l, I128b, U128l, U128b
+
+
+def test_i8():
+    assert I8.is_static()
+    assert I8.calc_size() == 1
+
+    assert I8.from_bytes(b"a", checked=True) == ord("a")
+    assert I8.from_bytes(bytes([200])) == -56
+    assert I8.from_bytes(b"ab") == ord("a")
+
+
+def test_u8():
+    assert U8.is_static()
+    assert U8.calc_size() == 1
+
+    assert U8.from_bytes(b"a", checked=True) == ord("a")
+    assert U8.from_bytes(bytes([200])) == 200
+    assert U8.from_bytes(b"ab") == ord("a")
+
+
+def test_i32l():
+    assert I32l.is_static()
+    assert I32l.calc_size() == 4
+
+    assert I32l.from_bytes(bytes([200, 0, 0, 0])) == 200
+    assert I32l.from_bytes(bytes([200, 0, 0, 210])) == (200 + 210 * 2 ** 24) - (2 ** 32)
+
+
+def test_i32b():
+    assert I32b.is_static()
+    assert I32b.calc_size() == 4
+
+    assert I32b.from_bytes(bytes([200, 0, 0, 0])) == 200 * 2 ** 24 - 2 ** 32
+    assert I32b.from_bytes(bytes([0, 0, 0, 210])) == 210
+
+
+def test_u32b():
+    assert U32b.is_static()
+    assert U32b.calc_size() == 4
+
+    assert U32b.from_bytes(bytes([200, 0, 0, 0])) == 200 * 2 ** 24
+    assert U32b.from_bytes(bytes([200, 0, 0, 210])) == (210 + 200 * 2 ** 24)
+
+
+def test_i128l():
+    assert I128l.is_static()
+    assert I128l.calc_size() == 16
+
+    assert I128l.from_bytes(b"\x80".ljust(16, b"\x00")) == 128
+    assert I128l.from_bytes(b"\x80".rjust(16, b"\x00")) == 2 ** 127 - 2 ** 128
+
+
+def test_u128l():
+    assert U128l.is_static()
+    assert U128l.calc_size() == 16
+
+    assert U128l.from_bytes(b"\x80".ljust(16, b"\x00")) == 128
+    assert U128l.from_bytes(b"\x80".rjust(16, b"\x00")) == 2 ** 127
+
+
+def test_i128b():
+    assert I128b.is_static()
+    assert I128b.calc_size() == 16
+
+    assert I128b.from_bytes(b"\x80".rjust(16, b"\x00")) == 128
+    assert I128b.from_bytes(b"\x80".ljust(16, b"\x00")) == 2 ** 127 - 2 ** 128
+
+
+def test_u128b():
+    assert U128b.is_static()
+    assert U128b.calc_size() == 16
+
+    assert U128b.from_bytes(b"\x80".rjust(16, b"\x00")) == 128
+    assert U128b.from_bytes(b"\x80".ljust(16, b"\x00")) == 2 ** 127
